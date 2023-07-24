049db5e836f61a70cbc22e2f9b40c1ccda78ee82
Oyawe <oyawegoodness@gmail.com>
2023-07-24 07:55:53 -0700

    useEffect Hook

--- src/Home.js
+++ src/Home.js
@@ -1,20 +1,36 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import BlogList from "./BlogList";
 
 const Home = () => {
-    // let name = "mario";
-    const [name, setName] = useState("mario");//it can be an array, object, boolean
-    const [age, setAge] = useState(25);
+const [blogs, setBlogs] = useState([
+    {title: 'My new Website', body: 'lorem ipsur...', author: 'mario', id: 1},
+    {title: 'Welcome Party!', body: 'lorem ipsur...', author: 'Jamie', id: 2},
+    {title: 'REACT dev Tools', body: 'lorem ipsur...', author: 'mario', id: 3}
+]);
 
-    const handleClick = (e) => {
-        setName("Jamie");
-        setAge(30);
-    }
+const [name, setName] = useState('mario');
 
+const handleDelete = (id) => {
+    const newBlogs = blogs.filter(blog => blog.id !== id);
+    setBlogs(newBlogs);
+}
+
+//useEffect hook fires on every render(any state change) 
+//dependency array -> an array that can be passed into the useEffect hook as a second argument //empty array means the useState runs at the first re-render
+
+useEffect(() => {
+    console.log('use effect ran')
+    // console.log(blogs)
+    console.log(name);
+}, [name]);
+
+//props
     return (
         <div className="home">
-            <h2>Homepage</h2>
-            <p>{name} is {age} years old</p>
-            <button onClick={handleClick}>Click Me</button>
+            <BlogList blogs={blogs} title="All Blogs!" handleDelete={handleDelete}/> 
+            {/* <BlogList blogs={blogs.filter((blog) => blog.author === 'mario')} title="Mario's Blogs!"/> */}
+            <button onClick={() => setName('luigi')}>change name</button>
+            <p>{name}</p>
         </div>
     );
 }
