f566c43b7f2ed699fc5b8e39deefea255302db2f
Oyawe <oyawegoodness@gmail.com>
2023-07-25 05:12:46 -0700

    Custom Hooks...yayyy

--- src/Home.js
+++ src/Home.js
@@ -1,36 +1,17 @@
-import { useState, useEffect } from "react";
+// import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
+import useFetch from "./useFetch";
 
 const Home = () => {
-const [blogs, setBlogs] = useState([
-    {title: 'My new Website', body: 'lorem ipsur...', author: 'mario', id: 1},
-    {title: 'Welcome Party!', body: 'lorem ipsur...', author: 'Jamie', id: 2},
-    {title: 'REACT dev Tools', body: 'lorem ipsur...', author: 'mario', id: 3}
-]);
-
-const [name, setName] = useState('mario');
-
-const handleDelete = (id) => {
-    const newBlogs = blogs.filter(blog => blog.id !== id);
-    setBlogs(newBlogs);
-}
-
-//useEffect hook fires on every render(any state change) 
-//dependency array -> an array that can be passed into the useEffect hook as a second argument //empty array means the useState runs at the first re-render
-
-useEffect(() => {
-    console.log('use effect ran')
-    // console.log(blogs)
-    console.log(name);
-}, [name]);
+    const {data: blogs, isPending, error} = useFetch('http://localhost:8000/blogs')
 
 //props
     return (
         <div className="home">
-            <BlogList blogs={blogs} title="All Blogs!" handleDelete={handleDelete}/> 
+            {error && <div>{error}</div>}
+            {isPending && <div>Loading...</div>}
+            {blogs && <BlogList blogs={blogs} title="All Blogs!"/>}
             {/* <BlogList blogs={blogs.filter((blog) => blog.author === 'mario')} title="Mario's Blogs!"/> */}
-            <button onClick={() => setName('luigi')}>change name</button>
-            <p>{name}</p>
         </div>
     );
 }
