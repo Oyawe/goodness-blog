7e19cc47e924a198f7c171812f3b42d3eeeadb0f
Oyawe <oyawegoodness@gmail.com>
2023-08-20 21:11:54 -0700

    fixed some bugs with the code

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-
+//Run this to start the JSON server to fetch the data
+npx json-server --watch data/db.json --port 8000
--- src/Home.js
+++ src/Home.js
@@ -3,14 +3,14 @@ import BlogList from "./BlogList";
 import useFetch from "./useFetch";
 
 const Home = () => {
-    const {data: blogs, isPending, error} = useFetch('http://localhost:8000/blogs')
+    const { data: blogs, isPending, error } = useFetch('http://localhost:8000/blogs')
 
-//props
+    //props
     return (
         <div className="home">
             {error && <div>{error}</div>}
             {isPending && <div>Loading...</div>}
-            {blogs && <BlogList blogs={blogs} title="All Blogs!"/>}
+            {blogs && <BlogList blogs={blogs} title="All Blogs!" />}
             {/* <BlogList blogs={blogs.filter((blog) => blog.author === 'mario')} title="Mario's Blogs!"/> */}
         </div>
     );
