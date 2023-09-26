47ae8348c1ff7301236ad82cdf0acbf2bb3eb21e
Oyawe <oyawegoodness@gmail.com>
2023-09-26 08:29:12 -0700

    made a few changes...damn i love this project

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,18 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useHistory, useParams } from 'react-router-dom/cjs/react-router-dom.min';
 import useFetch from './useFetch';
 
 const BlogDetails = () => {
     const { id } = useParams();
     const { data: blog, error, isPending } = useFetch('http://localhost:8000/blogs/' + id);
+    const [isLoading, setIsLoading] = useState(false);
     const history = useHistory();
 
     const handleClick = () => {
-        fetch('http://localhost:8000/blogs/' + blog.id, {
-            method: "DELETE"
-        }).then(() => {
-            history.push('/')
-        })
+
+        setIsLoading(true);
+
+        setTimeout(() => {
+            fetch('http://localhost:8000/blogs/' + blog.id, {
+                method: "DELETE"
+            }).then(() => {
+                setIsLoading(false);
+                history.push('/')
+            })
+        }, 1000);
     }
 
 
@@ -25,7 +32,8 @@ const BlogDetails = () => {
                     <h2>{blog.title}</h2>
                     <p>Written by {blog.author}</p>
                     <div>{blog.body}</div>
-                    <button onClick={handleClick}>Delete Blog</button>
+                    {!isLoading && <button onClick={handleClick}>Delete Blog</button>}
+                    {isLoading && <button onClick={handleClick} disabled>Deleting Blog...</button>}
                 </article>
             )}
         </div>
--- src/NotFound.js
+++ src/NotFound.js
@@ -4,12 +4,12 @@ import { Link } from "react-router-dom/cjs/react-router-dom.min";
 const NotFound = () => {
     return (
         <div className="not-found">
-            <h2>Sorry</h2>
+            <h2>Sorry !! ☹️</h2>
             <p>That page cannot be found</p>
             <div className="err-link">
-            <Link to='/'>
-                Back to the homepage...
-            </Link>
+                <Link to='/'>
+                    Back to the homepage...
+                </Link>
             </div>
         </div>
     );
